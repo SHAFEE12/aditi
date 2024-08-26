aadec94948e7fbe41daa036b7aa377c7dd60b8dd
SHAFEE12 <152318336+SHAFEE12@users.noreply.github.com>
2024-08-27 00:00:27 +0530

    Add files via upload

---
+++ bd/script.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const video = document.getElementById('bgVideo');
+
+//     // Add an event listener for when the video ends
+//     video.addEventListener('ended', () => {
+//         // Remove the video from the background
+//         video.parentNode.removeChild(video);
+
+//         // Change the background to white
+//         document.body.style.background = 'white';
+//     });
+// });
